4cc36f70acb673e89cf141d53b1d8009bce1ab4b
Cristopher1728 <168488565+Cristopher1728@users.noreply.github.com>
2026-08-02 10:54:06 -0500

    Actualizar termux.sh

--- termux.sh
+++ termux.sh
@@ -8,7 +8,8 @@ echo -e "\e[1;37;45m* 🌎 ┼─┼┼┼─>> Inicinado Instalación de paquet
 termux-setup-storage
 pkg update && pkg upgrade -y
 #Instalación Paquetes Necesarios
-pkg install x11-repo
+pkg install x11-repo -y
+pkg update
 pkg install xfce4 i3 tigervnc xorg-xrandr tilix ruby wget fastfetch vim htop w3m sc sc-im mc cmatrix rofi nitrogen lxappearance pipewire vlc-qt papirus-icon-theme cmatrix polybar neofetch htop xarchiver pcmanfm gimp curl -y
 
 #Colocando Password a servidor VNC . . .
