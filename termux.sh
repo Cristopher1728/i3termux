1e4f6c26ec373b590c4f8d7efb823d494e65e9a4
Cristopher1728 <168488565+Cristopher1728@users.noreply.github.com>
2026-08-02 11:02:57 -0500

    Actualizar termux.sh

--- termux.sh
+++ termux.sh
@@ -10,7 +10,7 @@ pkg update && pkg upgrade -y
 #Instalación Paquetes Necesarios
 pkg install x11-repo -y
 pkg update
-pkg install xfce4 i3 tigervnc xorg-xrandr tilix ruby wget fastfetch vim htop w3m sc sc-im mc cmatrix rofi nitrogen lxappearance pipewire vlc-qt papirus-icon-theme cmatrix polybar neofetch htop xarchiver pcmanfm gimp curl -y
+pkg install xfce4 i3 tigervnc xorg-xrandr ruby wget fastfetch vim htop w3m sc-im mc rofi nitrogen lxappearance pipewire papirus-icon-theme cmatrix htop xarchiver pcmanfm curl -y
 
 #Colocando Password a servidor VNC . . .
 echo -e "\e[1;37;45m* 🌎 ┼─┼┼┼─>> Colocando Password a servidor VNC,  . . . \e[1;39;49m"
@@ -35,12 +35,6 @@ mkdir /data/data/com.termux/files/home/.config/nitrogen
 mv bg-saved.cfg nitrogen.cfg /data/data/com.termux/files/home/.config/nitrogen
 
 
-echo -e "\e[1;37;45m* 🌎 ┼─┼┼┼─>> Instalando LOLCAT . . . \e[1;39;49m"
-wget https://github.com/busyloop/lolcat/archive/master.zip
-unzip master.zip
-cd lolcat-master/bin
-gem install lolcat
-
 echo -e "\e[1;37;45m* 🌎 ┼─┼┼┼─>> Integrando comandos en el archivo bash.bashrc. . . \e[1;39;49m"
 echo "
 
@@ -48,10 +42,10 @@ echo "
 export PS1='\[\033[01;35m\][\u\[\033[01;33m\]㉿\[\033[01;34m\]\@][\[\033[01;33m\]\w\[\033[01;34m\]]\[\033[01;32m\]\$\n┼┼》\[\033[01;37m\]'
 
 clear
-fastfetch |lolcat -as 550
+fastfetch
 alias sh1='cd $PREFIX/etc && nano -l bash.bashrc'
 alias up='pkg upd && pkg upg -y'
-alias fa='clear && fastfetch |lolcat -as 550'
+alias fa='clear && fastfetch'
 alias x='exit'
 alias i='cd ~ && ls -l'
 alias xfc='vncserver :1;export DISPLAY=:1;startxfce4 &'
@@ -68,9 +62,8 @@ x = salir de terminal (Cttol + d)
 i = Dirigirse a la carpeta Home - listado archivos
 xfc = Inicia entorno gráfico Xfce En el puerto Localhost:5901
 i3 = Inicia entorno gráfico i3wm En el puerto Localhost:5901
-=====================================================' |lolcat -as 550" >> $PREFIX/etc/bash.bashrc
+====================================================='" >> $PREFIX/etc/bash.bashrc
 
 clear
-cmatrix
-fastfetch |lolcat -as 550
+fastfetch
 echo -e "\e[1;37;45m* 🌎 ┼─┼┼┼─>> Instalación Finalizada, Disfrute del Sistema y Aplicaciones Instalados . . . \e[1;39;49m"
